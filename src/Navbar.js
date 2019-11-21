7481f3d74c6ef132f251799282a79294436d5682
Rakan <rakanaljamal@hotmail.com>
2019-11-21 11:26:06 +0200

    'FixingSearchButton'

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,57 +5,51 @@ import axios from 'axios'
 import "./Navbar.css"
 import "./anim.css"
 import ShowSearchedMovies from './ShowSearchedMovies';
+import { tsMethodSignature } from '@babel/types';
 class Navbar extends Component {
     constructor(props) {
         super(props);
         this.inputRef = React.createRef();
         this.state = {
-            hideSearch: true,
             searchValue: '',
             redirect: false,
-            searchResults: false,
             sResult: [],
             dataFetched: false
         };
     }
-    componentDidMount() {
-        window.addEventListener('click', (e) => {
-            let myDom = document.getElementById('search-box');
-            let onScreen = (e.screenX < 825 || e.screenX > 1200) ||
-                (e.screenY < 108 || e.screenY > 150)
-
-            if (myDom.offsetWidth > 0 && onScreen) {
-                myDom.className = 'hide'
-                this.setState({ hideSearch: true, searchResults: !this.state.searchResults })
-            }
-            this.state.searchResults ?
-                document.getElementById('search-results').className = 'py-4 show-search'
-                : document.getElementById('search-results').className = 'py-4 hide-search';
-
+    componentDidMount(){
+        window.addEventListener('click',()=>{
+            let valid =false;
+            let myDom = document.getElementById('search-results');
+            let insideDom=document.querySelectorAll(":hover");
+            insideDom.forEach(dom=>{
+                if(dom === document.getElementById('search-box') || dom === document.querySelector('.search-result'))
+                valid = true;
+            })
+            myDom.className= valid && this .state.searchValue ? 'search-result-haha':'search-result'
+            
         })
     }
-    componentDidUpdate(prevProps, prevState, snapshot) {
+
+    componentDidUpdate(prevProps, prevState) {
         if (prevState.searchValue !== this.state.searchValue) {
             let getMovies = async () => {
                 let result = await axios.get(`http://localhost:8080/api/smovie/${this.state.searchValue}?limit=4`);
                 this.setState({ sResult: result.data })
             }
             getMovies();
+            let myDom = document.getElementById('search-results');
+
+           myDom.className= this.state.searchValue && document.activeElement === document.getElementById('inside-search')  ? 'search-result-haha' : 'search-result' 
+        
         }
     }
 
     searchClicked = () => {
         let myDom = document.getElementById('search-box');
-        let searchResultsDom = document.getElementById('search-results');
-        searchResultsDom.className = !this.state.searchResults ? 'hide-search' : 'show-search'
-        myDom.className = !this.state.hideSearch ? 'hide' : 'show'
-        this.setState(prevState => (
-            { hideSearch: !this.state.hideSearch, searchResults: true }
-        ))
-        setTimeout(() => {
-            this.inputRef.current.focus()
-        }, 500)
-
+        let focusDom = document.getElementById('inside-search')
+        myDom.className = 'show'
+        focusDom.focus()
     }
     handleChange = (e) => {
 
@@ -80,6 +74,14 @@ class Navbar extends Component {
             <div className='loader'></div>
             <h1>Not found</h1>
         </>
+        let showMovieResutls = <div id='search-results' className='search-result'>
+            {this.state.sResult.length > 0 || this.state.searchValue === '' ?
+                this.showMoveis()
+                :
+                showLoader
+            } </div>
+
+        
 
         return (
             <>
@@ -96,22 +98,21 @@ class Navbar extends Component {
                             <i
                                 onClick={this.searchClicked}
                                 className="fas fa-search fa-2x"></i>
-                            <form id='search-box' className='hide' onSubmit={this.handleSubmit}>
-                                <input type="text" ref={this.inputRef} value={this.state.searchValue} onChange={this.handleChange} />
-                            </form>
+                                <div className="final-search">
+                                <form id='search-box' className='hide' onSubmit={this.handleSubmit}>
+                                    <input autoComplete="off" id='inside-search' type="text" value={this.state.searchValue} onChange={this.handleChange} />
+                                
+                                {showMovieResutls}    
+                                </form>                            
+                                </div>
+
                             <NavLink exact activeClassName="active-link" to="/">Home</NavLink >
                             <NavLink activeClassName="active-link" to="/movie/1">Top Rated</NavLink >
                             <NavLink activeClassName="active-link" to="/movie/2">Trending</NavLink >
                         </div>
                     </div>
                 </div >
-                <div id='search-results' className="py-4 hide-search">
-                    {this.state.sResult.length > 0 || this.state.searchValue === '' ?
-                        this.showMoveis()
-                        :
-                        showLoader
-                    }
-                </div>
+
             </>
         );
     }
