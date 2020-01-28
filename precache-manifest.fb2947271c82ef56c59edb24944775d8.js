c882186e49623950e5bcfe9070c72ce82a314eef
Rakan Aljamal <57729061+RakanAljamal@users.noreply.github.com>
2020-01-28 07:02:08 +0200

    Updates

---
+++ precache-manifest.fb2947271c82ef56c59edb24944775d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10009fee369d71a179974ff4292c68fc",
+    "url": "/Yallamovie/index.html"
+  },
+  {
+    "revision": "96b5b6dd6eb0afc794b1",
+    "url": "/Yallamovie/static/css/main.6b911b83.chunk.css"
+  },
+  {
+    "revision": "bbf148aa135f8bd7b1d1",
+    "url": "/Yallamovie/static/js/2.2c9f3477.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/Yallamovie/static/js/2.2c9f3477.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96b5b6dd6eb0afc794b1",
+    "url": "/Yallamovie/static/js/main.c6bcd3a1.chunk.js"
+  },
+  {
+    "revision": "a467cb899d47665cf9cf",
+    "url": "/Yallamovie/static/js/runtime-main.fafe740e.js"
+  }
+]);
